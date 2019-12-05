8d4b721478d9c9136ca167fb82ff12a67ee0aaef
GizemSoylemez <37050175+GizemSoylemez@users.noreply.github.com>
2019-12-05 15:50:42 +0300

    User-Profile

--- FindWorkerWeb.WebUI/Controllers/UserController.cs
+++ FindWorkerWeb.WebUI/Controllers/UserController.cs
@@ -10,41 +10,16 @@ namespace FindWorkerWeb.WebUI.Controllers
 {
     public class UserController : Controller
     {
-        private UserManager<UserModel> usermanager;
-        public UserController(UserManager<UserModel> _userManager)
-        {
-            usermanager = _userManager;
-        }
+        
         public IActionResult Index()
         {
             return View();
         }
-        [HttpGet]
-        public async Task<IActionResult> Profile()
+      
+        public IActionResult Profile()
         {
-            var user = await usermanager.GetUserAsync(User);
-            ViewBag.user = user;
             return View();
         }
-        [HttpGet]
-        public async Task<IActionResult> Profile(string username,string name, string password, string surname, string email, int phonenumber, string photo)
-        {
-            var user = await usermanager.GetUserAsync(User);
-            user.Email = email;
-            user.PhoneNumber = phonenumber.ToString();
-            user.UserName = username;
-            user.Name = name;
-            user.Password = password;
-            user.Surname = surname;
-            user.ProfilePhoto = photo;
-
-            var result = await usermanager.UpdateAsync(user);
-            if(!result.Succeeded)
-            {
-                return Redirect ("/");
-            }
-            ViewBag.user = user;
-            return View();
-        }
+       
     }
 }
